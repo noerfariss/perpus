ce8bc3966749d5c4e0e0d4a5ece134a21f1f16f2
NUR FARIS PRASTYO <noerfaris@gmail.com>
2023-04-03 14:36:23 +0700

    testing cronjob

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -15,7 +16,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('denda:traking')->dailyAt('18:30')->timezone(zona_waktu());
+        // $schedule->command('denda:traking')->dailyAt('18:30')->timezone(zona_waktu());
+        $schedule->call(function(){
+            Log::info('haloo');
+        })->everyMinute();
     }
 
     /**
