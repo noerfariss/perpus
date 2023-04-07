c01b8a0173a7c527b57e141b31fe9f1390c4652f
NUR FARIS PRASTYO <noerfaris@gmail.com>
2023-04-07 14:10:35 +0700

    fix switch s3 & local in SiswaModul

--- resources/views/backend/siswa/index.blade.php
+++ resources/views/backend/siswa/index.blade.php
@@ -21,7 +21,7 @@
                         @endpermission
 
                         @permission('siswa-print')
-                            {!! exportBtn(['data', 'foto'], route('ajax-siswa'), 'DATA siswa') !!}
+                            {!! exportBtn(['data', 'foto'], route('ajax-siswa'), 'DATA SISWA') !!}
                         @endpermission
                     </div>
                 </div>
@@ -33,7 +33,6 @@
                 <table class="table table-sm table-hover display nowrap mb-4" id="datatable">
                     <thead>
                         <tr>
-                            <th>NO</th>
                             <th>foto</th>
                             <th>siswa</th>
                             <th>ttl</th>
@@ -198,9 +197,7 @@
                     d.kelas = $('#kelas').val();
                 },
             },
-            columns: [{
-                    data: 'DT_RowIndex'
-                },
+            columns: [
                 {
                     data: 'foto'
                 },
